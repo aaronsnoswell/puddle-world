dc80a3f67acebddf4ea0a41aec996066bb42b4e1
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-05-12 10:54:43 +1000

    Fix typo with gym register ofr PuddleWorld-v0

--- puddle_world/envs/__init__.py
+++ puddle_world/envs/__init__.py
@@ -1,7 +1,5 @@
-
 from gym.envs.registration import register
 
 register(
-    id='PuddleWorldl-v0',
-    entry_point='plane_world.envs:PuddleWorldEnv',
+    id="PuddleWorld-v0", entry_point="plane_world.envs:PuddleWorldEnv",
 )
