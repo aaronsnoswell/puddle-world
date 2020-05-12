378620315cc5c04d2ee442ef2ddc4ea45dd2c2cc
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-05-12 10:54:58 +1000

    Add environments to module scope

--- puddle_world/envs/__init__.py
+++ puddle_world/envs/__init__.py
@@ -1,5 +1,8 @@
 from gym.envs.registration import register
 
+from puddle_world.envs.puddle_world_env import PuddleWorldEnv, CanonicalPuddleWorldEnv
+
+
 register(
     id="PuddleWorld-v0", entry_point="plane_world.envs:PuddleWorldEnv",
 )
